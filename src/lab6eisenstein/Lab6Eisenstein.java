6ed492233a0d86d0708586844dbffec1d6cef5ac
Theo Eisenstein <theo.eisenstein@gmail.com>
2019-01-17 16:56:04 -0500

    First Submission of Lab 6

---
+++ src/lab6eisenstein/Lab6Eisenstein.java
@@ -0,0 +1,30 @@
+package lab6eisenstein;
+
+import java.util.Random;
+
+import java.util.scanner;
+
+public class Lab6Eisenstein {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		scanner scnr = new Scanner(System.in);
+		
+		
+		
+		
+
+	}
+	
+	//Build Specification 1, static method named generateRandomDieRoll with a parameter int of dice sides
+	
+	public static int generateRandomDieRoll(int numberOfSides) {
+		
+		
+		//Random().nextInt(100)
+	}
+
+}
